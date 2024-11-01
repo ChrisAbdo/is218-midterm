91c672948281c4dd4c79667fd4fa71c7956e7ce9
Chris Abdo <chris.j.abdo@gmail.com>
2024-11-01 09:21:46 -0400

    pd history and csv partially functional

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -8,5 +8,7 @@ class MenuCommand(Command):
         print("- multiply")
         print("- divide")
         print("- history")
+        print("- pandas <csv_file_path> (WRITE TO CSV file)")
+        print("- pandas-history (display calculation history from pandas.csv)")
         print("- menu (display this menu)")
         print("- exit (quit the app)")
