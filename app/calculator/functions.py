19aa20a12b14cf77bcc33623bdd465029a1ccfe1
Chris Abdo <chris.j.abdo@gmail.com>
2024-10-31 21:10:27 -0400

    calc functions

---
+++ app/calculator/functions.py
@@ -0,0 +1,21 @@
+from typing import List
+from .calculations import Calculations
+
+class Calculations:
+    calculation_history: List[Calculations] = []
+
+    @classmethod
+    def register(cls, calculation: Calculations):
+        cls.calculation_history.append(calculation)
+
+    @classmethod
+    def history(cls) -> List[Calculations]:
+        return cls.calculation_history
+
+    @classmethod
+    def clear(cls):
+        cls.calculation_history.clear()
+
+    @classmethod
+    def last(cls) -> Calculations:
+        return cls.calculation_history[-1] if cls.calculation_history else None
